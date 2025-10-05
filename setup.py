42bd6dcb94e41f63abac03ec3897aa264a5c8a5b
Arunima Tiwari <tewariaru2001@gmail.com>
2025-10-06 00:18:32 +0530

    setting up setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+E_DOT='-e .'
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    this function will return the list of requirements!!
+
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+
+        if E_DOT in requirements:
+            requirements.remove(E_DOT)
+
+    return requirements
+
+
+
+
+
+setup(
+    name="mlproject",
+    version='0.0.1',
+    author="Arunima",
+    author_email="tewariaru2001@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
